0968fb07372859f7fe9dda78f8d83735672dbe37
Yolisma <yolisisabel28@gmail.com>
2023-12-22 12:30:58 -0800

    stuck on 'test the book model'

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  (async() =>{
+    await db.sequelize.sync({force: true});
+    try{
+      const books = await Book.findAll({
+        attributes: ['title', 'author', 'genre', 'year'],
+        return res.json(books);
+        console.log(json); 
+  })
+    } catch(error){
+      console.log(error)
+    }
+  })
+});
 module.exports = router;
+
+db.models.Book = require("./models/books.js")(sequelize);
