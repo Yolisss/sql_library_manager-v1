35d3dc2b7909be413bad0cde9b0033ea3f7d8817
Yolisma <yolisisabel28@gmail.com>
2023-12-26 23:31:29 -0800

    stuck on get request for new book added

--- routes/books.js
+++ routes/books.js
@@ -17,23 +17,28 @@ router.get("/", async function (req, res, next) {
 router.get("/new", async function (req, res, next) {
   res.render("new-book");
 });
+//2.req body returns an obj containing key/value pairs aka form data
+//ex if we console.log(req.body) we get props of form inputs =>
+//{title: "", author: "", genre: "", year=""}
+//so we'll pass req.body within create
 router.post("/new", async function (req, res, next) {
-  //nothing yet
+  //1.create is an async call that returns a promise
+  //and inside the async func, you use await key to wait for a promise
+  //create reqs an obj with props that map the model attributes
+  const book = await Book.create(req.body);
+  //when db builds and saves new book record, app should redirect to new book
+  //autoincrement is in place for id
+  res.redirect("/books/" + book.id);
 });
 router.get("/:id", async function (req, res, next) {
-  const bookId = parseFloat(req.params.id);
-  const book = books.find((book) => book.id === bookId);
-  console.log(books);
-  console.log(typeof bookId);
-  if (project) {
-    res.render("index");
-  } else {
-    const err = new Error();
-    err.status = 404;
-    err.message = "Sorry, project cannot be found";
-    console.log(message);
-    next(err);
-  }
+  //using SQL's findByPk method
+  //FindByPk is an async call that returns a promise whose resolved
+  //value is the single instance retrieved by they key or id value
+  //2. retrieve a book by passing findByPk the book id
+  //in express rroutes, you use route params to capture values specified
+  //in the URL path (req.params)
+  const book = await Book.findByPk(req.params.id);
+  res.render("/new");
 });
 router.post("/:id", async function (req, res, next) {
   //nothing yet
