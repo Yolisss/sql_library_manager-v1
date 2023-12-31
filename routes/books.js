ef0e91bda18a1786f8ad1f2d59a328ada05335be
Yolisma <yolisisabel28@gmail.com>
2023-12-31 15:01:22 -0800

    cleaned up code

--- routes/books.js
+++ routes/books.js
@@ -4,24 +4,18 @@ var router = express.Router();
 var { Book } = require("../models");
 var createError = require("http-errors");
 
+//shows full list of books
 router.get("/", async function (req, res, next) {
-  //any changes made to book module in code,
-  //the sync func is going to update the db to reflect
-  //those changes
-  // await db.sequelize.sync({ force: true });
   const books = await Book.findAll();
-  //console.log(books);
-  //ref index.pug => template; books as variable
   res.render("index", { books, title: "Books" });
 });
 
+//create new form
 router.get("/new", async function (req, res, next) {
   res.render("new-book");
 });
-//2.req body returns an obj containing key/value pairs aka form data
-//ex if we console.log(req.body) we get props of form inputs =>
-//{title: "", author: "", genre: "", year=""}
-//so we'll pass req.body within create
+
+//post a new book to db
 router.post("/new", async function (req, res, next) {
   let book;
   try {
@@ -42,13 +36,8 @@ router.post("/new", async function (req, res, next) {
   }
 });
 
+//show book detail form
 router.get("/:id", async function (req, res, next) {
-  //using SQL's findByPk method
-  //FindByPk is an async call that returns a promise whose resolved
-  //value is the single instance retrieved by they key or id value
-  //2. retrieve a book by passing findByPk the book id
-  //in express rroutes, you use route params to capture values specified
-  //in the URL path (req.params)
   const book = await Book.findByPk(req.params.id);
   if (book === null) {
     res.status(404);
@@ -59,6 +48,7 @@ router.get("/:id", async function (req, res, next) {
   }
 });
 
+//updates book info in db
 router.post("/:id", async function (req, res, next) {
   let book;
   try {
@@ -84,6 +74,7 @@ router.post("/:id", async function (req, res, next) {
   }
 });
 
+//deletes a book
 router.post("/:id/delete", async function (req, res, next) {
   const book = await Book.findByPk(req.params.id);
   if (book) {
